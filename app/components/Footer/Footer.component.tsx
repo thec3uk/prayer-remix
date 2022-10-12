872ae1cb4fc89644bb354f7e34a0849100d40a4c
RealJonParker <96420407+RealJonParker@users.noreply.github.com>
2022-10-12 18:50:13 +0100

    A few styling updates

--- app/components/Footer/Footer.component.tsx
+++ app/components/Footer/Footer.component.tsx
@@ -1,63 +1,70 @@
-import { Box, Flex, Text, Link } from '@chakra-ui/react';
-import { Link as RemixLink } from '@remix-run/react';
-import type { ReactNode } from 'react';
-import React from 'react';
-import { footerItems } from './Footer.definition';
+import { Box, Flex, Text, Link } from "@chakra-ui/react";
+import { Link as RemixLink } from "@remix-run/react";
+import type { ReactNode } from "react";
+import React from "react";
+import { footerItems } from "./Footer.definition";
 
 const FooterList = ({
-	title,
-	children,
+  title,
+  children,
 }: {
-	title: string;
-	children: ReactNode;
+  title: string;
+  children: ReactNode;
 }) => {
-	return (
-		<Flex columnGap={8}>
-			<Text as="h4" fontWeight="bold" size="lg" minW="40%">
-				{title}
-			</Text>
-			<Flex flexDir="column" mt="1">
-				{children}
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex columnGap={8}>
+      <Text as="h4" fontWeight="bold" size="lg" minW="40%">
+        {title}
+      </Text>
+      <Flex flexDir="column" mt="1">
+        {children}
+      </Flex>
+    </Flex>
+  );
 };
 
 function Footer() {
-	return (
-		<Box as="footer" w={'100vw'} mt={4} px={{ base: 3, md: 4 }}>
-			<Flex
-				flexDir={{ base: 'column', md: 'row' }}
-				justifyContent="space-between"
-				borderTop="1px solid black"
-				pt="4"
-			>
-				{footerItems.map(({ primary, fields }, idx) => {
-					return (
-						<FooterList key={primary.title} title={primary.title}>
-							{fields.map(link => {
-								return (
-									<React.Fragment key={link.link_title}>
-										<Link
-											key={link.link_title}
-											as={RemixLink}
-											to={link.link_url}
-											color="gray.500"
-											fontWeight="semibold"
-											size={{ base: 'xs', md: 'm' }}
-											_hover={{ color: 'red.500' }}
-										>
-											{link.link_title}
-										</Link>
-									</React.Fragment>
-								);
-							})}
-						</FooterList>
-					);
-				})}
-			</Flex>
-		</Box>
-	);
+  return (
+    <Box
+      as="footer"
+      w={"full"}
+      my={6}
+      px={{ base: 3, md: 4 }}
+      maxWidth="container.2xl"
+    >
+      <Flex
+        flexDir={{ base: "column", md: "row" }}
+        justifyContent="space-between"
+        borderTop="1px solid black"
+        pt="4"
+        gap={{ base: 4, md: 0 }}
+      >
+        {footerItems.map(({ primary, fields }, idx) => {
+          return (
+            <FooterList key={primary.title} title={primary.title}>
+              {fields.map((link) => {
+                return (
+                  <React.Fragment key={link.link_title}>
+                    <Link
+                      key={link.link_title}
+                      as={RemixLink}
+                      to={link.link_url}
+                      color="gray.500"
+                      fontWeight="semibold"
+                      size={{ base: "xs", md: "m" }}
+                      _hover={{ color: "red.500" }}
+                    >
+                      {link.link_title}
+                    </Link>
+                  </React.Fragment>
+                );
+              })}
+            </FooterList>
+          );
+        })}
+      </Flex>
+    </Box>
+  );
 }
 
 export default Footer;
--- app/components/PrayerCard/PrayerCard.tsx
+++ app/components/PrayerCard/PrayerCard.tsx
@@ -1,41 +1,35 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import type { IPrayerCardProps } from './PrayerCard.definition';
-import PrayerCardActions from './PrayerCardActions';
+import { Box, Flex, Text } from "@chakra-ui/react";
+import type { IPrayerCardProps } from "./PrayerCard.definition";
+import PrayerCardActions from "./PrayerCardActions";
 
 const PrayerCard = ({
-	data,
-	onAmen,
-	onReport,
-	featureView = false,
+  data,
+  onAmen,
+  onReport,
+  featureView = false,
 }: IPrayerCardProps) => {
-	const bgColor = data.type == 'praise' ? 'yellow.500' : 'teal.500';
+  const bgColor = data.type == "praise" ? "yellow.500" : "teal.500";
 
-	return (
-		<Box
-			w="100%"
-			mb={4}
-			display="inline-block"
-			bgColor="white"
-			boxShadow="0px 0px 2px rgba(0, 0, 0, 0.25)"
-		>
-			<Box w="100%" bgColor={bgColor} h={1}></Box>
-			<Flex flexDir={'column'} p={5} gap={2}>
-				<Text>{data.prayer}</Text>
-				<Text fontWeight="bold">
-					{`${data.name} ${
-						data.location ? ` (${data.location})` : ''
-					}`}
-				</Text>
-				{!featureView && (
-					<PrayerCardActions
-						data={data}
-						onAmen={onAmen}
-						onReport={onReport}
-					/>
-				)}
-			</Flex>
-		</Box>
-	);
+  return (
+    <Box
+      w="100%"
+      mb={4}
+      display="inline-block"
+      bgColor="white"
+      boxShadow="0px 0px 2px rgba(0, 0, 0, 0.25)"
+    >
+      <Box w="100%" bgColor={bgColor} h={1}></Box>
+      <Flex flexDir={"column"} p={5} gap={2}>
+        <Text>{data.prayer}</Text>
+        <Text fontWeight="bold">
+          {`${data.name} ${data.location ? ` (${data.location})` : ""}`}
+        </Text>
+        {!featureView && (
+          <PrayerCardActions data={data} onAmen={onAmen} onReport={onReport} />
+        )}
+      </Flex>
+    </Box>
+  );
 };
 
 export default PrayerCard;
