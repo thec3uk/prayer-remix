6cb83926ca51ab9932da7ba8208c1ddec049d48c
RealJonParker <96420407+RealJonParker@users.noreply.github.com>
2022-10-21 13:30:03 +0100

    Styling for dark mode

--- app/components/PrayerCard/PrayerCard.tsx
+++ app/components/PrayerCard/PrayerCard.tsx
@@ -1,46 +1,43 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import type { IPrayerCardProps } from './PrayerCard.definition';
-import PrayerCardActions from './PrayerCardActions';
+import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import type { IPrayerCardProps } from "./PrayerCard.definition";
+import PrayerCardActions from "./PrayerCardActions";
 
 const PrayerCard = ({
-	data,
-	onAmen,
-	onReport,
-	featureView = false,
+  data,
+  onAmen,
+  onReport,
+  featureView = false,
 }: IPrayerCardProps) => {
-	const bgColor = data.type == 'praise' ? 'yellow.500' : 'teal.500';
-	const fontSize = featureView ? 'xl' : 'md';
+  const bgColor = data.type == "praise" ? "yellow.500" : "teal.500";
+  const fontSize = featureView ? "xl" : "md";
 
-	return (
-		<Box
-			w="100%"
-			mb={4}
-			display="inline-block"
-			bgColor="white"
-			boxShadow="0px 0px 2px rgba(0, 0, 0, 0.25)"
-		>
-			<Box w="100%" bgColor={bgColor} h={1}></Box>
-			<Flex flexDir={'column'} p={5} gap={2}>
-				<Text size={fontSize}>{data.prayer}</Text>
+  return (
+    <Box
+      w="100%"
+      mb={4}
+      display="inline-block"
+      bgColor={useColorModeValue("gray.100", "gray.800")}
+      boxShadow={useColorModeValue(
+        "0px 0px 2px rgba(0, 0, 0, 0.25)",
+        "0px 0px 2px rgba(255, 255, 255, 0.25)"
+      )}
+    >
+      <Box w="100%" bgColor={bgColor} h={1}></Box>
+      <Flex flexDir={"column"} p={5} gap={2}>
+        <Text size={fontSize}>{data.prayer}</Text>
 
-				<Flex flexDir={'row'} justifyContent="space-between">
-					<Text fontWeight="bold">
-						{`${data.name} ${
-							data.location ? ` (${data.location})` : ''
-						}`}
-					</Text>
-					{featureView && <Text>{data.created_at}</Text>}
-				</Flex>
-				{!featureView && (
-					<PrayerCardActions
-						data={data}
-						onAmen={onAmen}
-						onReport={onReport}
-					/>
-				)}
-			</Flex>
-		</Box>
-	);
+        <Flex flexDir={"row"} justifyContent="space-between">
+          <Text fontWeight="bold">
+            {`${data.name} ${data.location ? ` (${data.location})` : ""}`}
+          </Text>
+          {featureView && <Text>{data.created_at}</Text>}
+        </Flex>
+        {!featureView && (
+          <PrayerCardActions data={data} onAmen={onAmen} onReport={onReport} />
+        )}
+      </Flex>
+    </Box>
+  );
 };
 
 export default PrayerCard;
