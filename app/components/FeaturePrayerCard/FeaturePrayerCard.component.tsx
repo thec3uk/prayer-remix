e4f92f6df1385f9b9621fdb057523458ddbb0650
Bradley Cornwell <bradley@isfluent.com>
2023-02-09 11:36:24 -0500

    Getting started with animations

--- app/components/FeaturePrayerCard/FeaturePrayerCard.component.tsx
+++ app/components/FeaturePrayerCard/FeaturePrayerCard.component.tsx
@@ -1,7 +1,8 @@
 import {
-	Box,
+	chakra,
 	Flex,
 	Icon,
+	shouldForwardProp,
 	Text,
 	useColorModeValue,
 	useDisclosure,
@@ -11,6 +12,8 @@ import PrayerHands from '../PrayerHands';
 import PrayerModal from '../PrayerModal';
 import type { IFeaturePrayerCardProps } from './FeaturePrayerCard.definition';
 import { BsPinAngle, BsPinFill } from 'react-icons/bs';
+import { motion, isValidMotionProp } from 'framer-motion';
+import { useState } from 'react';
 
 const FeaturePrayerCard = ({
 	data,
@@ -20,63 +23,145 @@ const FeaturePrayerCard = ({
 	const bgColor = data.type == 'praise' ? 'yellow.500' : 'teal.500';
 	const textColor = useColorModeValue('inherit', 'whiteAlpha.900');
 	const { isOpen, onClose, onOpen } = useDisclosure();
+	const ChakraBox = chakra(motion.div, {
+		shouldForwardProp: prop =>
+			isValidMotionProp(prop) || shouldForwardProp(prop),
+	});
+
+	const [content, setContent] = useState(false);
+	const cardVariant = {
+		back: {
+			x: 0,
+			rotateY: 0,
+			zIndex: 10,
+			transition: {
+				type: 'spring',
+				duration: 2.0,
+			},
+		},
+		front: {
+			x: 0,
+			rotateY: 180,
+			transition: {
+				type: 'spring',
+				duration: 2.0,
+			},
+		},
+	};
+
+	const handleClick = () => {
+		setContent(() => !content);
+	};
 
 	return (
 		<Flex
-			bgColor={useColorModeValue('white', 'gray.800')}
-			boxShadow={useColorModeValue(
-				'0px 0px 2px rgba(0, 0, 0, 0.25)',
-				'0px 0px 2px rgba(255, 255, 255, 0.25)'
-			)}
-			p={5}
-			alignContent={'space-between'}
-			borderTop={'0.25em solid'}
-			borderColor={bgColor}
-			_hover={{
-				background: 'white',
-				cursor: 'pointer',
-			}}
-			flexDirection="column"
-			justifyContent="space-between"
+			w={'100%'}
+			h={'100%'}
+			alignItems={'center'}
+			justifyContent="center"
+			position={'relative'}
 		>
-			<Flex justifyContent={'space-between'}>
-				{data.type == 'praise' ? (
-					<Praise w="24px" h="26px" />
-				) : (
-					<PrayerHands w="24px" h="26px" />
+			<ChakraBox
+				onClick={handleClick}
+				variants={cardVariant}
+				animate={content ? 'front' : ''}
+				initial={{ rotateY: 0, zIndex: 10 }}
+				sx={{
+					WebkitBackfaceVisibility: 'hidden',
+					backfaceVisibility: 'hidden',
+				}}
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Flex
+					bgColor={useColorModeValue('white', 'gray.800')}
+					boxShadow={useColorModeValue(
+						'0px 0px 2px rgba(0, 0, 0, 0.25)',
+						'0px 0px 2px rgba(255, 255, 255, 0.25)'
+					)}
+					p={5}
+					alignContent={'space-between'}
+					borderTop={'0.25em solid'}
+					borderColor={bgColor}
+					_hover={{
+						background: 'white',
+						cursor: 'pointer',
+					}}
+					flexDirection="column"
+					justifyContent="space-between"
+				>
+					<Flex justifyContent={'space-between'}>
+						{data.type == 'praise' ? (
+							<Praise w="24px" h="26px" />
+						) : (
+							<PrayerHands w="24px" h="26px" />
+						)}
+						{pinned ? (
+							<Icon
+								as={BsPinFill}
+								h="26px"
+								onClick={() => togglePin(!pinned)}
+							/>
+						) : (
+							<Icon
+								as={BsPinAngle}
+								h="26px"
+								onClick={() => togglePin(!pinned)}
+							/>
+						)}
+					</Flex>
+					<Text color={textColor} size={'xl'} mb={2} mt={2}>
+						{data.prayer}
+					</Text>
+					<Flex direction="row" justifyContent={'space-between'}>
+						<Text fontWeight="bold" color={textColor} mb={2}>
+							{`${data.name} ${
+								data.location ? ` (${data.location})` : ''
+							}`}
+						</Text>
+					</Flex>
+					<PrayerModal
+						prayer={data.prayer}
+						name={data.name}
+						location={data.location}
+						bgColor={bgColor}
+						isOpen={isOpen}
+						onClose={onClose}
+					></PrayerModal>
+				</Flex>
+			</ChakraBox>
+			<ChakraBox
+				onClick={handleClick}
+				variants={cardVariant}
+				animate={content ? 'back' : ''}
+				initial={{ rotateY: 180 }}
+				position={'absolute'}
+				h={'100%'}
+				w={'100%'}
+				bgColor={'teal.500'}
+				textColor={'white'}
+				sx={{
+					WebkitBackfaceVisibility: 'hidden',
+					backfaceVisibility: 'hidden',
+				}}
+				boxShadow={useColorModeValue(
+					'0px 0px 2px rgba(0, 0, 0, 0.25)',
+					'0px 0px 2px rgba(255, 255, 255, 0.25)'
 				)}
-				{pinned ? (
-					<Icon
-						as={BsPinFill}
-						h="26px"
-						onClick={() => togglePin(!pinned)}
-					/>
-				) : (
-					<Icon
-						as={BsPinAngle}
-						h="26px"
-						onClick={() => togglePin(!pinned)}
-					/>
-				)}
-			</Flex>
-			<Text onClick={onOpen} color={textColor} size={'xl'} mb={2} mt={2}>
-				{data.prayer}
-			</Text>
-			<Flex direction="row" justifyContent={'space-between'}>
-				<Text fontWeight="bold" color={textColor} mb={2}>
-					{`${data.name} ${
-						data.location ? ` (${data.location})` : ''
-					}`}
-				</Text>
-			</Flex>
-			<PrayerModal
-				prayer={data.prayer}
-				name={data.name}
-				location={data.location}
-				bgColor={bgColor}
-				isOpen={isOpen}
-				onClose={onClose}
-			></PrayerModal>
+			>
+				<Flex
+					alignContent={'space-between'}
+					borderTop={'0.25em solid'}
+					p={5}
+					borderColor={'white'}
+					flexDirection="column"
+					justifyContent="space-between"
+				>
+					More information can go on the back?!
+					{JSON.stringify(content)}
+				</Flex>
+			</ChakraBox>
 		</Flex>
 	);
 };
