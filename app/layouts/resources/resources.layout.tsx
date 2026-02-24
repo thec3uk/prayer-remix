351dd3488f93df9c5954f871a2d8487794f72850
Andrew Miller <info@akmiller.co.uk>
2026-02-24 19:45:31 +0000

    Allow for youtube shorts

--- app/layouts/resources/resources.layout.tsx
+++ app/layouts/resources/resources.layout.tsx
@@ -15,7 +15,7 @@ import type { IResourcesProps } from "./resources.definition";
 
 function getYouTubeId(url: string): string | null {
   const match = url.match(
-    /(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    /(?:youtube\.com\/(?:watch\?v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
   );
   return match ? match[1] : null;
 }
